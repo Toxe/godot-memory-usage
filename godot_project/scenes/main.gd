cb162b8a502a44dea1b6f6e9cb1ae5f0af2a36fb
Toxe <toxe@toxe.net>
2026-09-10 17:43:42 +0200

    Measure objects

--- godot_project/scenes/main.gd
+++ godot_project/scenes/main.gd
@@ -2,40 +2,33 @@ extends Control
 
 var _scene: PackedScene = null
 var _instance: Node = null
+var _object: Object = null
 
 @onready var _timer: Timer = $Timer
 
 
-func _on_measure_load_scenes_button_pressed() -> void:
-    print("============================================================")
-    await measure_scene("res://scenes/test_scenes/empty_node.tscn")
-    await measure_scene("res://scenes/test_scenes/empty_node2d.tscn")
-    await measure_scene("res://scenes/test_scenes/empty_control.tscn")
-    assert(Performance.get_monitor(Performance.OBJECT_ORPHAN_NODE_COUNT) == 0)
-
-
-func wait_and_get_memory() -> int:
+func _wait_and_get_memory() -> int:
     _timer.start();
     await _timer.timeout
     return int(Performance.get_monitor(Performance.MEMORY_STATIC))
 
 
-func measure_scene(path: String) -> void:
-    var _throw_away := await wait_and_get_memory()
-    var m0 := await wait_and_get_memory()
+func _measure_scene(path: String) -> void:
+    var _throw_away := await _wait_and_get_memory()
+    var m0 := await _wait_and_get_memory()
 
     _scene = load(path)
-    var m1 := await wait_and_get_memory()
+    var m1 := await _wait_and_get_memory()
 
     _instance = _scene.instantiate()
-    var m2 := await wait_and_get_memory()
+    var m2 := await _wait_and_get_memory()
 
     _instance.queue_free()
     _instance = null
-    var m3 := await wait_and_get_memory()
+    var m3 := await _wait_and_get_memory()
 
     _scene = null
-    var m4 := await wait_and_get_memory()
+    var m4 := await _wait_and_get_memory()
 
     var scene_size1 := m1 - m0
     var scene_size2 := m3 - m4
@@ -48,3 +41,38 @@ func measure_scene(path: String) -> void:
         print("%s: scene=%d, instance=%d" % [path, scene_size1, instance_size1])
     else:
         print("%s: ERROR, something went wrong, scene: %d vs. %d, instance: %d vs. %d" % [path, scene_size1, scene_size2, instance_size1, instance_size2])
+
+
+func _measure_object() -> void:
+    var _throw_away := await _wait_and_get_memory()
+    var m0 := await _wait_and_get_memory()
+
+    _object = Object.new()
+    var m1 := await _wait_and_get_memory()
+
+    _object.free()
+    _object = null
+    var m2 := await _wait_and_get_memory()
+
+    var object_size1 := m1 - m0
+    var object_size2 := m1 - m2
+    var object_size_diff := object_size1 - object_size2
+
+    if object_size_diff == 0:
+        print("object=%d" % [object_size1])
+    else:
+        print("ERROR, something went wrong, object: %d vs. %d" % [object_size1, object_size2])
+
+
+func _on_measure_load_scenes_button_pressed() -> void:
+    print("============================================================")
+    await _measure_scene("res://scenes/test_scenes/empty_node.tscn")
+    await _measure_scene("res://scenes/test_scenes/empty_node2d.tscn")
+    await _measure_scene("res://scenes/test_scenes/empty_control.tscn")
+    assert(Performance.get_monitor(Performance.OBJECT_ORPHAN_NODE_COUNT) == 0)
+
+
+func _on_measure_objects_button_pressed() -> void:
+    print("============================================================")
+    await _measure_object()
+    assert(Performance.get_monitor(Performance.OBJECT_ORPHAN_NODE_COUNT) == 0)
