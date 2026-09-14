5f918475ebe26a877950879939bfd712c4d51d92
Toxe <toxe@toxe.net>
2026-09-14 10:50:25 +0200

    Simplify code

--- godot_project/scenes/main.gd
+++ godot_project/scenes/main.gd
@@ -3,8 +3,6 @@ extends Control
 var _scene: PackedScene = null
 var _instance: Node = null
 var _object: Object = null
-var _refcounted: RefCounted = null
-var _resource: Resource = null
 
 @onready var _timer: Timer = $Timer
 
@@ -40,74 +38,41 @@ func _measure_scene(path: String) -> void:
     var instance_size_diff := instance_size1 - instance_size2
 
     if scene_size_diff == 0 && instance_size_diff == 0:
-        print("%s: scene=%d, instance=%d" % [path, scene_size1, instance_size1])
+        print("%s, scene: %d, instance: %d" % [path, scene_size1, instance_size1])
     else:
-        print("%s: ERROR, something went wrong, scene: %d vs. %d, instance: %d vs. %d" % [path, scene_size1, scene_size2, instance_size1, instance_size2])
+        print("%s, ERROR, something went wrong, scene: %d vs. %d, instance: %d vs. %d" % [path, scene_size1, scene_size2, instance_size1, instance_size2])
 
 
-func _measure_object() -> void:
+func _measure_type(type: Variant) -> void:
     var _throw_away := await _wait_and_get_memory()
     var m0 := await _wait_and_get_memory()
 
-    _object = Object.new()
+    @warning_ignore("unsafe_method_access")
+    _object = type.new()
     var m1 := await _wait_and_get_memory()
 
-    _object.free()
+    var type_name := _get_type_name(_object)
+    var m2 := await _wait_and_get_memory()
+
+    if _object is not RefCounted:
+        _object.free()
     _object = null
-    var m2 := await _wait_and_get_memory()
+    var m3 := await _wait_and_get_memory()
 
     var size1 := m1 - m0
-    var size2 := m1 - m2
-    var size_diff := size1 - size2
-
-    if size_diff == 0:
-        print("object=%d" % [size1])
+    var size2 := m2 - m3
+    if size1 - size2 == 0:
+        print("%s: %d" % [type_name, size1])
     else:
-        print("ERROR, something went wrong, object: %d vs. %d" % [size1, size2])
+        print("ERROR, something went wrong, %s: %d vs. %d" % [type_name, size1, size2])
+    assert(Performance.get_monitor(Performance.OBJECT_ORPHAN_NODE_COUNT) == 0)
 
 
-func _measure_refcounted() -> void:
-    var _throw_away := await _wait_and_get_memory()
-    var m0 := await _wait_and_get_memory()
-
-    _refcounted = RefCounted.new()
-    var m1 := await _wait_and_get_memory()
-
-    _refcounted = null
-    var m2 := await _wait_and_get_memory()
-
-    var size1 := m1 - m0
-    var size2 := m1 - m2
-    var size_diff := size1 - size2
-
-    if size_diff == 0:
-        print("refcounted=%d" % [size1])
-    else:
-        print("ERROR, something went wrong, refcounted: %d vs. %d" % [size1, size2])
-
-
-func _measure_resource() -> void:
-    var _throw_away := await _wait_and_get_memory()
-    var m0 := await _wait_and_get_memory()
-
-    _resource = Resource.new()
-    var m1 := await _wait_and_get_memory()
-
-    _resource = null
-    var m2 := await _wait_and_get_memory()
-
-    var size1 := m1 - m0
-    var size2 := m1 - m2
-    var size_diff := size1 - size2
-
-    if size_diff == 0:
-        print("resource=%d" % [size1])
-    else:
-        print("ERROR, something went wrong, resource: %d vs. %d" % [size1, size2])
+func _get_type_name(type: Variant) -> String:
+    return str(type).get_slice("<", 1).get_slice("#", 0)
 
 
 func _on_measure_load_scenes_button_pressed() -> void:
-    print("============================================================")
     await _measure_scene("res://scenes/test_scenes/empty_node.tscn")
     await _measure_scene("res://scenes/test_scenes/empty_node2d.tscn")
     await _measure_scene("res://scenes/test_scenes/empty_control.tscn")
@@ -115,18 +80,12 @@ func _on_measure_load_scenes_button_pressed() -> void:
 
 
 func _on_measure_objects_button_pressed() -> void:
-    print("============================================================")
-    await _measure_object()
-    assert(Performance.get_monitor(Performance.OBJECT_ORPHAN_NODE_COUNT) == 0)
+    await _measure_type(Object)
 
 
 func _on_measure_ref_counted_button_pressed() -> void:
-    print("============================================================")
-    await _measure_refcounted()
-    assert(Performance.get_monitor(Performance.OBJECT_ORPHAN_NODE_COUNT) == 0)
+    await _measure_type(RefCounted)
 
 
 func _on_measure_resources_button_pressed() -> void:
-    print("============================================================")
-    await _measure_resource()
-    assert(Performance.get_monitor(Performance.OBJECT_ORPHAN_NODE_COUNT) == 0)
+    await _measure_type(Resource)
